705aa6c6f9f81dbab28a4eb2e299be2e17e024ca
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-10 11:05:41 +0800

    created a while loop that starts when the user says yes

--- Calculator.py
+++ Calculator.py
@@ -2,6 +2,6 @@
 #Asks the user to if ready to start (loop)
 start=input("Welcome to the calculator! Would you like to start now? Y/N")
     #Asks the user what operation to be used
-    
+while start.upper()=="Y":
     #Asks user the two different numbers to do an operation with
     #If division, asks the user if they want to see the whole number only or the remainder only
