994e82b3470d55fa7ecf9f2e55f48c91799f57e6
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-10 11:25:53 +0800

    fixed the exception handling structure

--- Calculator.py
+++ Calculator.py
@@ -7,7 +7,12 @@ while start.upper() == "Y":
     #Asks user the two different numbers to do an operation with
     try:
         first_operand=float(input("Input your first operand:"))
+    except ValueError:
+        print("The value you entered is not a number. Please try again.")
+        first_operand=float(input("Input your first operand:"))
+    try:
         second_operand=float(input("Input your second operand:"))
     except ValueError:
         print("The value you entered is not a number. Please try again.")
+        second_operand=float(input("Input your second operand:"))
     #If division, asks the user if they want to see the whole number only or the remainder only
