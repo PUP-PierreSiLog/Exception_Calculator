0e8470b428a0a3159c7305ea96b92cb536281776
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-10 12:33:26 +0800

    created a while loop that loops if the user inputs an invalid operation.

--- Calculator.py
+++ Calculator.py
@@ -3,7 +3,13 @@
 start=input("Welcome to the calculator! Would you like to start now? Y/N")
 while start.upper() == "Y":
     #Asks the user what operation to be used
-    operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division.")
+    operation=["A", "S", "M", "D"]
+    while True:
+        operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division.")
+        if operation.upper()==operation:
+            break
+        else:
+            print("Invalid operation!")
     #Asks user the two different numbers to do an operation with
     while True:
         try:
@@ -11,10 +17,10 @@ while start.upper() == "Y":
             break
         except ValueError:
             print("The value you entered is not a number, please try again.")
-    try:
-        second_operand=float(input("Input your second operand:"))
-    except ValueError:
-        print("The value you entered is not a number. Please try again.")
-    else:
-        break
-    #If division, asks the user if they want to see the whole number only or the remainder only
+    while True:
+        try:
+            second_operand=float(input("Input your second operand:"))
+            break
+        except ValueError:
+            print("The value you entered is not a number. Please try again.")
+        #If division, asks the user if they want to see the whole number only or the remainder only
