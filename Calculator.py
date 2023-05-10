7d156b8a8afae279ad91a632437baf0c4242c4a5
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-10 11:20:34 +0800

    created the first exemption handling. Error reasks the user to redo the input of the operands.

--- Calculator.py
+++ Calculator.py
@@ -5,6 +5,9 @@ while start.upper() == "Y":
     #Asks the user what operation to be used
     operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division.")
     #Asks user the two different numbers to do an operation with
-    first_operand=float(input("Input your first operand:"))
-    second_operand=float(input("Input your second operand:"))
+    try:
+        first_operand=float(input("Input your first operand:"))
+        second_operand=float(input("Input your second operand:"))
+    except ValueError:
+        print("The value you entered is not a number. Please try again.")
     #If division, asks the user if they want to see the whole number only or the remainder only
