4055ac9af0943f2aa0915b01ed010d7806db7330
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-10 11:02:00 +0800

    created the start variable which prompts the user to start the program

--- Calculator.py
+++ Calculator.py
@@ -1,5 +1,7 @@
 #Welcome Screen
 #Asks the user to if ready to start (loop)
+start=input("Welcome to the calculator! Would you like to start now? Y/N")
     #Asks the user what operation to be used
+    
     #Asks user the two different numbers to do an operation with
     #If division, asks the user if they want to see the whole number only or the remainder only
