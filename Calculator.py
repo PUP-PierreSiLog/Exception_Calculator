5fdf6714b2a91df1f50e8c025262f2ceb9cf5de7
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-10 10:56:33 +0800

    created basic structure

---
+++ Calculator.py
@@ -0,0 +1,5 @@
+#Welcome Screen
+#Asks the user to if ready to start (loop)
+    #Asks the user what operation to be used
+    #Asks user the two different numbers to do an operation with
+    #If division, asks the user if they want to see the whole number only or the remainder only
