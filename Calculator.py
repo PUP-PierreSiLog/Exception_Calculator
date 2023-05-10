effd92f6a01b2b8e3bed7d15ab6f0a49b07e8013
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-10 17:45:22 +0800

    Added the operation, created another line of code that makes the operation to uppercase

--- Calculator.py
+++ Calculator.py
@@ -3,10 +3,11 @@
 start=input("Welcome to the calculator! Would you like to start now? Y/N")
 while start.upper() == "Y":
     #Asks the user what operation to be used
-    operation=["A", "S", "M", "D"]
+    operation_list=["A", "S", "M", "D"]
     while True:
         operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division.")
-        if operation.upper()==operation:
+        operation=operation.upper()
+        if operation in operation_list:
             break
         else:
             print("Invalid operation!")
@@ -23,4 +24,17 @@ while start.upper() == "Y":
             break
         except ValueError:
             print("The value you entered is not a number. Please try again.")
-        #If division, asks the user if they want to see the whole number only or the remainder only
+    #Does the chosen operation
+    if "A" in operation:
+        result=first_operand+second_operand
+        print(result)
+    if "M" in operation:
+        result=first_operand*second_operand
+        print(result)
+    if "S" in operation:
+        result=first_operand-second_operand
+        print(result)
+    if "D" in operation:
+        result=first_operand/second_operand
+        print(result)
+    #If division, asks the user if they want to see the whole number only or the remainder only
