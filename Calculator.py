a9cc72f76a0008bc10666a1925f6dbddfaba4322
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-10 12:09:08 +0800

    fixed the exception handling structure. Created an exception inside a while loop.

--- Calculator.py
+++ Calculator.py
@@ -5,14 +5,16 @@ while start.upper() == "Y":
     #Asks the user what operation to be used
     operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division.")
     #Asks user the two different numbers to do an operation with
-    try:
-        first_operand=float(input("Input your first operand:"))
-    except ValueError:
-        print("The value you entered is not a number. Please try again.")
-        first_operand=float(input("Input your first operand:"))
+    while True:
+        try:
+            first_operand=float(input("Please input your first operand:"))
+            break
+        except ValueError:
+            print("The value you entered is not a number, please try again.")
     try:
         second_operand=float(input("Input your second operand:"))
     except ValueError:
         print("The value you entered is not a number. Please try again.")
-        second_operand=float(input("Input your second operand:"))
+    else:
+        break
     #If division, asks the user if they want to see the whole number only or the remainder only
