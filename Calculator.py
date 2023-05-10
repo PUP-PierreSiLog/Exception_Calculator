720007e6e031f8f2f6027e04d7a7029f8376fea7
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-10 11:12:46 +0800

    created a prompt that asks the user what operations to be used

--- Calculator.py
+++ Calculator.py
@@ -1,7 +1,11 @@
 #Welcome Screen
 #Asks the user to if ready to start (loop)
 start=input("Welcome to the calculator! Would you like to start now? Y/N")
+while start.upper() == "Y":
     #Asks the user what operation to be used
-while start.upper()=="Y":
+    operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division.")
+    
     #Asks user the two different numbers to do an operation with
     #If division, asks the user if they want to see the whole number only or the remainder only
+else:
+    break
