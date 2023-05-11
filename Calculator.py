a0e726ddd23b686e3f5d4ebea9242bb3ab2369ac
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-05-11 08:29:16 +0800

    Added design on the welcome screen

--- Calculator.py
+++ Calculator.py
@@ -1,12 +1,14 @@
 #Welcome Screen
 import sys
+import pyfiglet
 #Asks the user to if ready to start (loop)
-start=input("Welcome to the calculator! Would you like to start now? Y/N")
+print(pyfiglet.figlet_format("Welcome to the calculator! ", justify="center"))
+start=input("Would you like to start now? Y/N: ")
 while start.upper() == "Y":
     #Asks the user what operation to be used
     operation_list=["A", "S", "M", "D"]
     while True:
-        operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division.")
+        operation=input("Please input an operation to use. Type the letters indicated for each operation: A for Addition, M for multiplication, S for subtraction, D for division: ")
         operation=operation.upper()
         if operation in operation_list:
             break
